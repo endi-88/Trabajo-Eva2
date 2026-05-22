a945ab915578da8d1e9c0b2c9f973a8196cb3c39
endi-88 <yerk.perez@duocuc.cl>
2026-05-22 01:46:32 +0000

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
-
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "FROM python:3.10-slim" >> tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 8888" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
-
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 8888:8888 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
